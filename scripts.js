16d50475936158c5f01d6c16a02e3f5340b4e208
MaxBayly <maxbayly@gmail.com>
2020-03-15 13:07:14 +1100

    convert to ts

--- scripts.js
+++
@@ -1,7 +0,0 @@
-
-function changeBackgroundColour() {
-	var colour = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-	document.body.style.background = colour;
-	document.getElementById("colourText").innerHTML = colour;
-
-}
