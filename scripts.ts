93c0c92b41f1c43436f86b00a21cd542cd693d2c
MaxBayly <maxbayly@gmail.com>
2020-03-18 19:20:24 +1100

    add storage of previous colour

--- scripts.ts
+++ scripts.ts
@@ -1,7 +1,16 @@
 
+var prevColour = "#FFFFFF";
 function changeBackgroundColour() {
-	var colour = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	localStorage.setItem("prevColour", prevColour)
+	var colour = '#'+(Math.random()*0xFFFFFF<<0).toString(16).toUpperCase();
+	prevColour = colour;
 	document.body.style.background = colour;
-	document.getElementById("colourText").innerHTML = colour;
+	document.getElementById("colourButton").innerHTML = colour;	
+}
+
+function getPreviousColour() {
+	var colour = localStorage.getItem("prevColour");
+	document.getElementById("colourButton").innerHTML = colour;
+	document.body.style.background = colour;
 
 }
